cd485f9cdd60274ad883ba1ee624f46fbac0a2c1
Gaina Xenia <xenia.gaina@mail.ru>
2020-05-06 17:12:27 +0300

    done

---
+++ src/app/components/lessons/component/detailsOnLesson/details-on-lesson.component.ts
@@ -0,0 +1,44 @@
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+} from "@angular/core";
+
+@Component({
+  selector: "details-on-lesson",
+  templateUrl: "./details-on-lesson.component.html",
+  styles: [
+    `
+      .pointer {
+        cursor: pointer;
+        color: gray;
+      }
+    `,
+  ],
+})
+export class DetailsOnLessonComponent {
+  public editFields = { name: false, hour: false, homework: false };
+  @ViewChild("nameInput", { static: false }) name;
+  @ViewChild("hourInput", { static: false }) hour;
+  @ViewChild("homeworkInput", { static: false }) homework;
+  @Input() oneLesson: any;
+  @Input() lessonIndex: number;
+  @Output() delete = new EventEmitter();
+
+  onDeleteLesson() {
+    this.delete.emit(this.lessonIndex);
+  }
+
+  onEditLesson(field) {
+    this.editFields[field] = true;
+    setTimeout(() => {
+      this[field].nativeElement.focus();
+    }, 0);
+  }
+
+  onBlur(field) {
+    this.editFields[field] = false;
+  }
+}
---
+++ src/app/components/lessons/component/lessonDetails/lessons-details.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Lesson } from "../../../../models/lesson.model";
+
+@Component({
+  selector: "app-lessons-details",
+  templateUrl: "./lessons-details.component.html",
+  styleUrls: ["./lessons-details.component.scss"],
+})
+export class LessonsDetailsComponent {
+  @Input() lessons: Lesson[];
+  @Output() add = new EventEmitter();
+  @Output() lessonClick = new EventEmitter();
+
+  onSubmit(lessonForm: NgForm) {
+    const formValue = lessonForm.value;
+    this.add.emit(formValue);
+    lessonForm.resetForm({});
+  }
+  onClickLesson(index: number) {
+    this.lessonClick.emit(index);
+  }
+}
